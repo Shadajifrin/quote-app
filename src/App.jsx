0d1afbf83358be7749139ca0dd1e32d4192ed0a1
Shadajifrin <fathimashadanp@gmail.com>
2023-11-16 10:53:50 +0530

    fetch quatable api

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
-import { useState } from 'react'
+import React,{ useState,useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [quote, setQuote] = useState("");
+  const[author,setAuthor]= useState("");
+  //http://api.quotable.io/random
+  useEffect(() =>{
+  fetch("http://api.quotable.io/random")
+  .then(res=>res.json())
+  .then(
+    (quote) =>{
+     setQuote(quote.content);
+     setAuthor(quote.author);
+     console.log(quote);
+    }
+  )
+  },[]);
 
+   
   return (
   <div className='App'>
     <div className='quote'>
-      <h2>New Qoute</h2>
-      <small>-Auther</small>
-    </div>
+      <h2>{quote}</h2>
+      <small>{author}</small>
+    </div><br />
     <button className='btn'>Generate new quote</button>
   </div>
   )
